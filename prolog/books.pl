eb0cffc93beb1b10ac164e94b678450be98e1763
Richard Garber <richard.garber@ufl.edu>
2023-10-24 18:30:05 -0400

    Finished Day 1 of Prolog

---
+++ prolog/books.pl
@@ -0,0 +1,32 @@
+author(brandon_sanderson).
+author(jaroslav_hacek).
+author(homer).
+author(virgil).
+author(hannu_rajaniemi).
+author(neal_stephenson).
+author(jrr_tolkien).
+author(mary_shelley).
+book(the_way_of_kings).
+book(words_of_radiance).
+book(oathbringer).
+book(rhythm_of_war).
+book(the_brave_soldier_svejk).
+book(iliad).
+book(odyssey).
+book(the_quantum_thief).
+book(cryptonomicon).
+book(lord_of_the_rings).
+book(the_silmarillion).
+book(frankenstein).
+wrote(brandon_sanderson, the_way_of_kings).
+wrote(brandon_sanderson, words_of_radiance).
+wrote(brandon_sanderson, oathbringer).
+wrote(brandon_sanderson, rhythm_of_war).
+wrote(jaroslav_hacek, the_brave_soldier_svejk).
+wrote(homer, iliad).
+wrote(homer, odyssey).
+wrote(hannu_rajaniemi, the_quantum_thief).
+wrote(neal_stephenson, cryptonomicon).
+wrote(jrr_tolkien, lord_of_the_rings).
+wrote(jrr_tolkien, the_silmarillion).
+wrote(mary_shelley, frankenstein).
---
+++ prolog/musicians.pl
@@ -0,0 +1,102 @@
+genre('Alina Kobialka', classical).
+genre('Andi Zhang', classical).
+genre('Anna Rabinova', classical).
+genre('Audrey Wright', classical).
+genre('Brian May', rock).
+genre('Charles Mingus', jazz).
+genre('Charlie Parker', jazz).
+genre('Christopher Martin', classical).
+genre('Dasol Jeong', classical).
+genre('David Gilmour', rock).
+genre('Dizzy Gillespie', jazz).
+genre('Duke Ellington', jazz).
+genre('Duoming Ba', classical).
+genre('Eddie Van Halen', rock).
+genre('Elizabeth Zeltser', classical).
+genre('Eric Clapton', rock).
+genre('Ethan Bensdorf', classical).
+genre('Fiona Simon', classical).
+genre('Frank Huang', classical).
+genre('Hae-Young Ham', classical).
+genre('Hannah Choi', classical).
+genre('Herbie Hancock', jazz).
+genre('Hyunju Lee', classical).
+genre('I-Jung Huang', classical).
+genre('Jeff Beck', rock).
+genre('Jimi Hendrix', rock).
+genre('Jimmy Page', rock).
+genre('Jin Suk Yu', classical).
+genre('John Coltrane', jazz).
+genre('Kerry McDermott', classical).
+genre('Kuan Cheng Lu', classical).
+genre('Kyung Ji Min', classical).
+genre('Lisa Eunsoo Kim', classical).
+genre('Lisa GiHae Kim', classical).
+genre('Louis Armstrong', jazz).
+genre('Marié Schwalbach', classical).
+genre('Matthew Muckey', classical).
+genre('Michelle Kim', classical).
+genre('Miles Davis', jazz).
+genre('Na Sun', classical).
+genre('Ornette Coleman', jazz).
+genre('Qianqian Li', classical).
+genre('Quan Ge', classical).
+genre('Sharon Yamada', classical).
+genre('Sheryl Staples', classical).
+genre('Sister Rosetta Tharpe', rock).
+genre('Soohyun Kwon', classical).
+genre('Su Hyun Park', classical).
+genre('Thelonious Monk', jazz).
+genre('Thomas Smith', classical).
+genre('Yulia Ziskel', classical).
+instrument('Alina Kobialka', violin).
+instrument('Andi Zhang', violin).
+instrument('Anna Rabinova', violin).
+instrument('Audrey Wright', violin).
+instrument('Brian May', guitar).
+instrument('Charles Mingus', bass).
+instrument('Charlie Parker', saxophone).
+instrument('Christopher Martin', trumpet).
+instrument('Dasol Jeong', violin).
+instrument('David Gilmour', guitar).
+instrument('Dizzy Gillespie', trumpet).
+instrument('Duke Ellington', piano).
+instrument('Duoming Ba', violin).
+instrument('Eddie Van Halen', guitar).
+instrument('Elizabeth Zeltser', violin).
+instrument('Eric Clapton', guitar).
+instrument('Ethan Bensdorf', trumpet).
+instrument('Fiona Simon', violin).
+instrument('Frank Huang', violin).
+instrument('Hae-Young Ham', violin).
+instrument('Hannah Choi', violin).
+instrument('Herbie Hancock', piano).
+instrument('Hyunju Lee', violin).
+instrument('I-Jung Huang', violin).
+instrument('Jeff Beck', guitar).
+instrument('Jimi Hendrix', guitar).
+instrument('Jimmy Page', guitar).
+instrument('Jin Suk Yu', violin).
+instrument('John Coltrane', saxophone).
+instrument('Kerry McDermott', violin).
+instrument('Kuan Cheng Lu', violin).
+instrument('Kyung Ji Min', violin).
+instrument('Lisa Eunsoo Kim', violin).
+instrument('Lisa GiHae Kim', violin).
+instrument('Louis Armstrong', trumpet).
+instrument('Marié Schwalbach', violin).
+instrument('Matthew Muckey', trumpet).
+instrument('Michelle Kim', violin).
+instrument('Miles Davis', trumpet).
+instrument('Na Sun', violin).
+instrument('Ornette Coleman', trumpet).
+instrument('Qianqian Li', violin).
+instrument('Quan Ge', violin).
+instrument('Sharon Yamada', violin).
+instrument('Sheryl Staples', violin).
+instrument('Sister Rosetta Tharpe', guitar).
+instrument('Soohyun Kwon', violin).
+instrument('Su Hyun Park', violin).
+instrument('Thelonious Monk', piano).
+instrument('Thomas Smith', trumpet).
+instrument('Yulia Ziskel', violin).
